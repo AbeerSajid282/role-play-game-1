8e254b0ee8c2204d1ba72707ff0829e743d2cb1b
AbeerSajid282 <63706796+AbeerSajid282@users.noreply.github.com>
2024-02-09 20:47:33 -0600

    Update code_1.js

--- code_1.js
+++ code_1.js
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>RPG - Dragon Repeller</title>
-  <link rel="stylesheet" type="text/css" href="https://www.google.com">
-</head>
-<body>
-  <div id="game"></div>
-</body>
+<html lang = "en">
+  <head>
+   <meta charset="UTF-8">
+   <title> RPGA - Dragon Repeller Abeer </title>
+<link rel="stylesheet" type="text/css" href="https://www.google.com">
+
+     </head>
+     <body>
+       <div id = "game"></div>
+       </body>
 </html>
